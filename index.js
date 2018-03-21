cb726d6bc2657831944c98fd5be3a69730b12c77
remery426 <emeryryan1@gmail.com>
2018-03-20 22:15:42 -0700

    build path

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
   const path  = require('path');
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'main', 'build', 'index.html'))
   });
 }
 const PORT = process.env.PORT || 5000;
